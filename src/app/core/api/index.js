f4848c049d111ba6051390116abb7fffd932f1ce
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2024-05-17 14:49:16 +0100

    add api

--- src/app/core/api/index.js
+++ src/app/core/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 const api_local="http://localhost:3000/api/v1"
 const api_cloud="https://api-chinua.onrender.com/api/v1/"
+
 const api= axios.create ({
-    baseURL:`http://localhost:3000/api/v1`
+    baseURL:`https://chinua-backend.onrender.com/api/v1`
 })
 
 export default api;
